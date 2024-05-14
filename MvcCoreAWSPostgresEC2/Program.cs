73d91173063574cf8f219d33f9526543a24dcb58
RazvanLucacii <rlucaci48@gmail.com>
2024-05-14 10:20:40 +0200

    Add project files.

---
+++ MvcCoreAWSPostgresEC2/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using MvcCoreAWSPostgresEC2.Data;
+using MvcCoreAWSPostgresEC2.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddTransient<RepositoryDepartamento>();
+string connectionString = builder.Configuration.GetConnectionString("PostgresSql");
+builder.Services.AddDbContext<DepartamentosContext>(options => options.UseNpgsql(connectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ MvcCoreAWSPostgresEC2/Repositories/RepositoryDepartamento.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreAWSPostgresEC2.Data;
+using MvcCoreAWSPostgresEC2.Models;
+using System.Diagnostics;
+
+namespace MvcCoreAWSPostgresEC2.Repositories
+{
+    public class RepositoryDepartamento
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamento(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<Departamento> GetDepartamentoAsync(int iddepartamento)
+        {
+            return await this.context.Departamentos.FirstOrDefaultAsync(z => z.IdDepartamento == iddepartamento);
+        }
+
+        public async Task InsertDepartamentoAsync(int id, string nombre, string localidad)
+        {
+            Departamento dept = new Departamento
+            {
+                IdDepartamento = id,
+                Nombre = nombre,
+                Localidad = localidad
+            };
+            this.context.Departamentos.Add(dept);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
